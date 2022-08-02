1c73fa715cc98381c5091ad610b8a890ece3ccf0
Alexandre Tondello <alexandre.tondello@descomplica.com.br>
2022-08-01 22:23:00 -0300

    First commit

---
+++ uuid.js
@@ -0,0 +1,10 @@
+import { v1 as uuidv1 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+
+console.log('');
+console.log('V1:');
+console.log(uuidv1().toUpperCase());
+console.log('');
+console.log('V4:');
+console.log(uuidv4().toUpperCase());
+console.log('');
